3e0dd665b2575092432f9c14147b4199640cdbe8
Thiago Pessoa Araujo <tp102000@gmail.com>
2025-11-15 12:53:59 -0300

    Hand Tracking: Desenvolvido a abertura de webcam e reconhecimento dos pontos nas mãos

---
+++ HandTracking/hand_tracking.py
@@ -0,0 +1,28 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands()
+
+cam = cv2.VideoCapture(0)
+resolution_x, resolution_y = 1280, 720
+
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, resolution_x)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, resolution_y)
+
+while True:
+    success, frame = cam.read()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(frame_rgb)
+    
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+    
+    cv2.imshow("Webcam", frame)
+    
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
